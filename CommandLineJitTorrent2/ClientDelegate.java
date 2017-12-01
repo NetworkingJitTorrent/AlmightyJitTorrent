83f7358c205baf7a24b2013c4f36804064295890
Nicholas Miller <nickbryanmiller@gmail.com>
2017-12-01 09:49:11 -0500

    make new branch because git being difficult

---
+++ CommandLineJitTorrent2/ClientDelegate.java
@@ -0,0 +1,14 @@
+public interface ClientDelegate {
+
+    Message onServerHandshakeReceived(Message message, int serverPeerID);
+
+    Message onServerBitfieldReceived(Message message, int serverPeerID);
+
+    Message onChokeReceived(Message message, int serverPeerID);
+
+    Message onUnChokeReceived(Message message, int serverPeerID);
+
+    Message onHaveReceived(Message message, int serverPeerID);
+
+    Message onPieceReceived(Message message, int serverPeerID);
+}
