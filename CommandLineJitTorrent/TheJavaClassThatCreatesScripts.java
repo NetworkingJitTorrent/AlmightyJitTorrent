b1a6735955fb6d4246fdf7ab2a2468d87d3534eb
bmtreuherz <bmtreuherz@ufl.edu>
2017-12-01 17:11:22 -0500

    Made new scripts but havn't tested yet

---
+++ CommandLineJitTorrent/TheJavaClassThatCreatesScripts.java
@@ -0,0 +1,184 @@
+import java.io.PrintWriter;
+import java.util.List;
+import static java.lang.System.exit;
+
+public class TheJavaClassThatCreatesScripts {
+
+    private static final String PROJECT_DIRECTORY = System.getProperty("user.dir");
+    private static final String COMMON_CONFIG = "Common.cfg";
+    private static final String PEER_INFO = "PeerInfo.cfg";
+    private static String username;
+
+    public static void main(String[] args){
+
+        CommonConfig commonConfig = loadCommonConfig(COMMON_CONFIG);
+        List<PeerInfo> peerInfo = loadPeerInfo(PEER_INFO);
+
+        // Validate we are given the correct number of args.
+        if (args.length == 1) {
+
+          // Create Scripts to run remotely
+          username = args[0];
+
+          try{
+              makeDirectoriesScript(peerInfo);
+              makeSetupRemoteScript(peerInfo);
+              makeRunRemotecript(peerInfo);
+              bringThemBoysHome(peerInfo, commonConfig.getFileName());
+          } catch(Exception e){
+              e.printStackTrace();
+          }
+        } else {
+          // Create scripts to run locally
+          try{
+            makeDirectoriesScript(peerInfo);
+            makeSetupLocalScript(peerInfo);
+            makeRunLocalScript(peerInfo);
+          }
+
+        }
+    }
+
+    public static void makeDirectoriesScript(List<PeerInfo> peerInfo) throws Exception{
+      PrintWriter writer = new PrintWriter("ScriptCreateDirectories", "UTF-8");
+      writer.println("#!/bin/bash\n");
+
+      writer.println("echo Creating Peer Directories\n");
+
+      for (int i=0; i < peerInfo.size(); i++){
+          writer.println("mkdir peer_" + peerInfo.get(i).getPeerID());
+      }
+
+      writer.close();
+    }
+
+    public static void makeSetupLocalScript(List<PeerInfo> peerInfo) throws Exception{
+        PrintWriter writer = new PrintWriter("ScriptSetupLocalEnvironments", "UTF-8");
+        writer.println("#!/bin/bash\n");
+
+        writer.println("echo Cleaning Directory\nrm *.class");
+        writer.println("echo Compiling Directory\njavac PeerProcess.java");
+        writer.close();
+    }
+
+    public static void makeSetupRemoteScript(List<PeerInfo> peerInfo) throws Exception{
+        PrintWriter writer = new PrintWriter("ScriptSetupRemoteEnvironments", "UTF-8");
+        writer.println("#!/bin/bash\n");
+
+        writer.println("echo Cleaning Directory\nrm *.class");
+        writer.println("echo Compiling Directory\njavac PeerProcess.java");
+        writer.println("echo Zipping Project\ntar -zcvf project.tgz .");
+
+        writer.println("echo Copying Project Over");
+
+        for (int i=0; i < peerInfo.size(); i++){
+            writer.println("osascript -e 'tell application \"Terminal\" to do script \"cd " + PROJECT_DIRECTORY + "; scp project.tgz " + username +  "@" + peerInfo.get(i).getHostName() + ":~/Documents/\"'\n");
+            writer.println("sleep 6s");
+        }
+        writer.close();
+    }
+    public static void makeRunLocalScript(List<PeerInfo> peerInfo) throws Exception{
+        PrintWriter writer = new PrintWriter("ScriptRunLocalPeers", "UTF-8");
+        writer.println("#!/bin/bash\n");
+
+        writer.println("echo Running Peers\n");
+
+        for (int i=0; i < peerInfo.size(); i++){
+            String scriptName = "ScriptLocalPeer" + i;
+            createLocalScriptPeer(scriptName, peerInfo.get(i), i);
+
+            writer.println("osascript -e 'tell application \"Terminal\" to do script \"cd " + PROJECT_DIRECTORY + "; chmod 700 " + scriptName + "; ./" + scriptName + "\"'");
+            writer.println("sleep 10s");
+        }
+
+        writer.close();
+    }
+
+    public static void makeRunRemoteScript(List<PeerInfo> peerInfo) throws Exception{
+        PrintWriter writer = new PrintWriter("ScriptRunRemotePeers", "UTF-8");
+        writer.println("#!/bin/bash\n");
+
+        writer.println("echo Running Peers\n");
+
+        for (int i=0; i < peerInfo.size(); i++){
+            String scriptName = "ScriptRemotePeer" + i;
+            createRemoteScriptPeer(scriptName, peerInfo.get(i), i);
+
+            writer.println("osascript -e 'tell application \"Terminal\" to do script \"cd " + PROJECT_DIRECTORY + "; chmod 700 " + scriptName + "; ./" + scriptName + "\"'");
+            writer.println("sleep 10s");
+        }
+
+        writer.close();
+    }
+
+    public static void createRemoteScriptPeer(String name, PeerInfo peerInfo, int i) throws Exception{
+        PrintWriter writer = new PrintWriter(name, "UTF-8");
+
+        writer.println("#!/bin/bash\n");
+
+        if (i == 0) {
+            writer.println("ssh " + username + "@" + peerInfo.getHostName() + " \"cd Documents/; tar -zxvf project.tgz; rm -rf logging; java PeerProcess " + peerInfo.getPeerID() + "\"");
+        } else {
+            writer.println("ssh " + username + "@" + peerInfo.getHostName() + " \"cd Documents/; tar -zxvf project.tgz; java PeerProcess " + peerInfo.getPeerID() + "\"");
+        }
+        writer.close();
+    }
+
+    public static void createLocalScriptPeer(String name, PeerInfo peerInfo, int i) throws Exception{
+        PrintWriter writer = new PrintWriter(name, "UTF-8");
+
+        writer.println("#!/bin/bash\n");
+
+        if (i == 0) {
+            writer.println("cd " + PROJECT_DIRECTORY);
+            writer.println("rm -rf logging");
+            writer.println("java PeerProcess " + peerInfo.getPeerID());
+        } else {
+            writer.println("cd " + PROJECT_DIRECTORY);
+            writer.println("java PeerProcess " + peerInfo.getPeerID());
+        }
+        writer.close();
+    }
+
+    public static void bringThemBoysHome(List<PeerInfo> peerInfo, String fileName) throws Exception{
+        PrintWriter writer = new PrintWriter("ScriptBringThemBoysHome", "UTF-8");
+        writer.println("#!/bin/bash\n");
+
+        writer.println("echo Bringing Them Boys Home\n");
+
+        for (int i=0; i < peerInfo.size(); i++){
+            writer.println("osascript -e 'tell application \"Terminal\" to do script \"cd " + PROJECT_DIRECTORY + "; scp " + username +"@"+ peerInfo.get(i).getHostName()
+            +":~/Documents/logging/log_peer_"+peerInfo.get(i).getPeerID()+".log "+ PROJECT_DIRECTORY +"; scp "+ username+"@"+ peerInfo.get(i).getHostName()
+            +":~/Documents/peer_"+ peerInfo.get(i).getPeerID() +"/"+peerInfo.get(i).getPeerID() + fileName  +" "+ PROJECT_DIRECTORY
+            + "/peer_"+peerInfo.get(i).getPeerID()+"/\"'\n");
+            writer.println("sleep 12s");
+        }
+        writer.close();
+    }
+
+    // Load the common config file
+    private static CommonConfig loadCommonConfig(String filename){
+        CommonConfig commonConfig = null;
+        try{
+            commonConfig = CommonConfig.createFromFile(filename);
+        } catch (Exception e){
+            e.printStackTrace();
+            exit(1);
+        }
+
+        return commonConfig;
+    }
+
+    // Load the peer info file
+    private static List<PeerInfo> loadPeerInfo(String filename){
+        List<PeerInfo> peerInfo = null;
+        try {
+            peerInfo = PeerInfo.createFromFile(filename);
+        } catch (Exception e){
+            e.printStackTrace();
+            exit(1);
+        }
+
+        return peerInfo;
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # **Welcome to the AlmightyJitTorrent!**
 
-This is a simplified version of BitTorrent. It can be run with a common config file and from two different peer info config files and has both an Intellij Project and Command Line Project.
+This is a simplified version of BitTorrent. It can be run with a common config file and a peer info config files and has both an Intellij Project and Command Line Project.
 
 ----------
 
@@ -8,7 +8,7 @@ This is a simplified version of BitTorrent. It can be run with a common config f
 
 **Description**
 
-The first config file is the ***Common.cfg***. Everything will use this file and it needs to have all the information below (excluding the values).
+The first config file is the ***Common.cfg***. Everything will use this file and it needs to have the following format:
 
 > **Example:**
 
@@ -21,11 +21,12 @@ The first config file is the ***Common.cfg***. Everything will use this file and
 
 ----
 
-## **PeerInfo.cfg and PeerInfoRemote.cfg**
+## **PeerInfo.cfg**
 
 **Description**
 
-These are used by two different files to create the peers. If you are running on localhosts then you need to use ***PeerInfo.cfg*** and compile/run ***StartTestEnv.java***. If you are running remote peers then you will need to use ***PeerInfoRemote.cfg***, compile/run ***TheJavaClassThatCreatesScripts.java*** (where the command line argument is your username for the remote machines), and then run three scripts (***ScriptSetupEnvironment***, ***ScriptRunRemotePeers***, and ***ScriptBringThemBoysHome***).
+This file contains information about all of the peers that will be in the system. It should have the following format:
+
 
 **File Content Organization**
 
@@ -33,37 +34,6 @@ The first attribute is the PeerID.
 The second attribute is the host name.
 The third attribute is the port number.
 The fourth attribute is a 1 if the peer contains the file or a 0 if not.
-
-### **PeerInfo.cfg**
-
-**Description**
-
-The first type of peer info config file is the ***PeerInfo.cfg***. This is the config used for a localhost, or a  non-remote session. You can test it by compiling and running ***StartTestEnv.java***.
-
-> **Example:**
-
-> - 1001 localhost 6008 1
-> - 1002 localhost 6009 0
-> - 1003 localhost 6010 0
-> - 1004 localhost 6011 0
-> - 1005 localhost 6012 0
-> - 1006 localhost 6013 0
-
-### **PeerInfoRemote.cfg**
-
-**Description**
-
-The second type of peer info config file is the ***PeerInfoRemote.cfg***. This is the config used for remote sessions.
-
-**How To Run**
-
-1. Compile ***TheJavaClassThatCreatesScripts.java*** with the command:
-	* javac TheJavaClassThatCreatesScripts.java
-2. Run ***TheJavaClassThatCreatesScripts.java*** with the command:
-	* java TheJavaClassThatCreatesScripts [insert your username]
-3. Run ***ScriptSetupEnvironment*** to clean, build, zip, and copy files over to the remote machines.
-4. Run ***ScriptRunRemotePeers*** to start the remote sessions and run the program.
-5. Run ***ScriptBringThemBoysHome*** to copy the log files and downloaded files from the remote machine to the current machine.
 
 > **Example:**
 
@@ -75,5 +45,19 @@ The second type of peer info config file is the ***PeerInfoRemote.cfg***. This i
 > - 1006 lin309-07.cise.ufl.edu 5095 0
 
 ----
+
+**How To Run**
+
+1. Compile ***TheJavaClassThatCreatesScripts.java*** with the command:
+	* `javac TheJavaClassThatCreatesScripts.java`
+2. Run ***TheJavaClassThatCreatesScripts.java*** with the command:
+	* `java TheJavaClassThatCreatesScripts [username] [common config filename] [peer info filename]`
+3. Run ***ScriptMakeDirectories*** to make all the peer_[peerID] directories.
+4. For any peers that will already have the file on startup, place the file into the appropriate peer_[peerID] directories.
+5. Run ***ScriptSetupEnvironment*** to clean, build, zip, and copy files over to the remote machines.
+6. Run ***ScriptRunRemotePeers*** to start the remote sessions and run the program.
+7. Run ***ScriptBringThemBoysHome*** to copy the log files and downloaded files from the remote machine to the current machine.
+
+NOTE: If you are getting a permission error when running these scripts, you may need to run `CHMOD 700 [filename]`
 
 Thanks for reading and have fun! :smiley:
